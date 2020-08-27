c59b5cd11061fe5b9ea76642e2e0c3508917903f
Ko-Gyeongtae <kokt020@naver.com>
2020-08-27 21:50:40 +0900

    login.py login screen ui code

---
+++ venv/login.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'C:\Users\kokt0\PycharmProjects\untitled\venv\login.ui'
+#
+# Created by: PyQt5 UI code generator 5.9.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName("Dialog")
+        Dialog.resize(960, 540)
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap("icon.jpg"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        Dialog.setWindowIcon(icon)
+        self.label = QtWidgets.QLabel(Dialog)
+        self.label.setGeometry(QtCore.QRect(0, 0, 960, 540))
+        self.label.setText("")
+        self.label.setPixmap(QtGui.QPixmap("background.jpg"))
+        self.label.setObjectName("label")
+        self.label_2 = QtWidgets.QLabel(Dialog)
+        self.label_2.setGeometry(QtCore.QRect(190, 220, 91, 41))
+        font = QtGui.QFont()
+        font.setFamily("한컴 고딕")
+        font.setPointSize(20)
+        self.label_2.setFont(font)
+        self.label_2.setObjectName("label_2")
+        self.label_3 = QtWidgets.QLabel(Dialog)
+        self.label_3.setGeometry(QtCore.QRect(190, 270, 101, 31))
+        font = QtGui.QFont()
+        font.setFamily("한컴 고딕")
+        font.setPointSize(20)
+        self.label_3.setFont(font)
+        self.label_3.setObjectName("label_3")
+        self.lineEdit_2 = QtWidgets.QLineEdit(Dialog)
+        self.lineEdit_2.setGeometry(QtCore.QRect(320, 230, 221, 20))
+        self.lineEdit_2.setObjectName("lineEdit_2")
+        self.lineEdit_3 = QtWidgets.QLineEdit(Dialog)
+        self.lineEdit_3.setGeometry(QtCore.QRect(320, 280, 221, 20))
+        self.lineEdit_3.setObjectName("lineEdit_3")
+        self.login = QtWidgets.QPushButton(Dialog)
+        self.login.setGeometry(QtCore.QRect(560, 280, 75, 23))
+        self.login.setObjectName("login")
+
+        self.retranslateUi(Dialog)
+        self.login.clicked.connect(Dialog.push_button_clicked)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        _translate = QtCore.QCoreApplication.translate
+        Dialog.setWindowTitle(_translate("Dialog", "로그인"))
+        self.label_2.setText(_translate("Dialog", "아이디"))
+        self.label_3.setText(_translate("Dialog", "회원가입"))
+        self.login.setText(_translate("Dialog", "로그인"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    Dialog = QtWidgets.QDialog()
+    ui = Ui_Dialog()
+    ui.setupUi(Dialog)
+    Dialog.show()
+    sys.exit(app.exec_())
+
